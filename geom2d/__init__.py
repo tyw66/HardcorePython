eb2a515207963398a13a5c22a364d1a971faa4b5
tyw <tyw66_git@qq.com>
2025-11-16 17:05:21 +0800

    [7.4]Geom2d的最后一步

---
+++ geom2d/__init__.py
@@ -0,0 +1,20 @@
+from . import nums
+from .size import Size
+from .tparam import *
+from .interpolation import *
+from .open_interval import OpenInterval
+
+from .point import Point
+from .vector import Vector
+from .segment import Segment
+from .line import Line
+from .polygon import Polygon
+from .circle import Circle  
+from .rect import Rect
+from .affine_transf import AffineTransform
+
+from .vectors import *
+from .circles import *
+from .rects import *
+from .polygons import *
+from .affine_transformers import *
---
+++ run_test.sh
@@ -0,0 +1,1 @@
+python3 -m unittest discover -s geom2d/tests/ -p '*_test.py'
